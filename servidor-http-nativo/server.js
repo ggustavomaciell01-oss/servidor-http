5d89424da83f144c5dec3216ce8a5dbfbeb5570f
ggustavomaciell01-oss <gustavo.martinez@estudante.ifms.edu.br>
2026-08-14 21:11:01 +0000

    add: novas rotas adicionadas

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -1,21 +1,40 @@
 import http from 'node:http'
+import { URL } from 'node:url'
 
-const porta = 3000
-
-const periodo = new Date().toISOString()
+const porta = 3000;
 
 const server = http.createServer();
 
-server.on('request', (req, res) =>{
-    console.log(`Servidor funcionando! ${req.method} ${req.url}`);
-    console.log(`no periodo ${periodo}`);
-    
-
-    res.statusCode = 201
+const requisicao = (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({"status" : "OK"}));
-});
+    res.statusCode = 200
+    const urlObj = new URL(req.url, `http://${req.headers.host}`);
+
+    if (req.method === 'GET' && urlObj.pathname === '/saudacao') {
+        const nome = urlObj.searchParams.get('nome');
+        return res.end(JSON.stringify({"nome" : nome}));
+    } else if (req.method === 'GET' && urlObj.pathname === '/') {
+        return res.end(JSON.stringify({"data" : "Esta é a página inicial"}));
+
+    } else if (req.method === 'GET' && urlObj.pathname === '/contato') {
+        return res.end(JSON.stringify({"data" : [{"telefone" : "67 99999 9999", "e-mail" : "email@gmail.com"}]}));
+
+    } else if (req.method === 'GET' && urlObj.pathname === '/produtos') {
+        return res.end(JSON.stringify({"data" : [{"Frango" : "R$67,00", "Arroz" : "R$52,00", "Batata" : "R$34,00"}]}));
+
+    }
+
+    
+    return res.end(JSON.stringify({ "chave": "valor" }));
+    
+    
+    console.log(`Requisição recebida! ${req.method} ${req.url}`);
+    res.end();
+}
+
+
+server.on('request', requisicao);
 
 server.listen(porta, () => {
-    console.log(`Servidor funcionando na porta ${porta} `)
-});
+    console.log(`Servidor ouvindo na porta ${porta}`)
+});			
