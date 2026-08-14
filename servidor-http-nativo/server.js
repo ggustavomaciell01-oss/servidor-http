a144c82bb49cf25a7ca55b23a9aa6bd8bb120c2f
ggustavomaciell01-oss <gustavo.martinez@estudante.ifms.edu.br>
2026-08-14 18:27:21 +0000

    edit:  exercicio 2

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -2,10 +2,14 @@ import http from 'node:http'
 
 const porta = 3000
 
+const periodo = new Date().toISOString()
+
 const server = http.createServer();
 
 server.on('request', (req, res) =>{
     console.log(`Servidor funcionando! ${req.method} ${req.url}`);
+    console.log(`no periodo ${periodo}`);
+    
 
     res.statusCode = 201
     res.setHeader('Content-Type', 'text/plain', 'charset: utf-8');
@@ -13,5 +17,5 @@ server.on('request', (req, res) =>{
 });
 
 server.listen(porta, () => {
-    console.log(`Servidor funcionando na porta ${porta}`)
+    console.log(`Servidor funcionando na porta ${porta} `)
 });
