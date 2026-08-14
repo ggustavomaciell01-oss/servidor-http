f988df8b2477701c35a717dfeb455cd8a1b2be87
ggustavomaciell01-oss <gustavo.martinez@estudante.ifms.edu.br>
2026-08-14 18:29:44 +0000

    edit: exercicio 3

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -12,8 +12,8 @@ server.on('request', (req, res) =>{
     
 
     res.statusCode = 201
-    res.setHeader('Content-Type', 'text/plain', 'charset: utf-8');
-    res.end("Recurso criado");
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({"status" : "OK"}));
 });
 
 server.listen(porta, () => {
